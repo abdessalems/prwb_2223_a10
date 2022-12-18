ca2ca54d49d01471f0a57fff82dadbe1f21d123a
Baganda49 <abdessalemsaa@gmail.com>
2022-12-18 22:16:28 +0100

    login

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -1,12 +1,38 @@
 <?php
 
-require_once 'model/Member.php';
+require_once 'model/user.php';
 require_once 'framework/View.php';
 require_once 'framework/Controller.php';
 
 class ControllerMain extends Controller {
+
+    //si l'utilisateur est connecté, redirige vers son profil.
+    //sinon, produit la vue d'accueil.
     public function index() : void {
-        echo "<h1>Hey !</h1>";
+        if ($this->user_logged()) {
+            $this->redirect("user", "full_name");
+        } else {
+            (new View("login"))->show();
+        }
     }
+
+    //gestion de la connexion d'un utilisateur
+    public function login() : void {
+        $mail = '';
+        $password = '';
+        $errors = [];
+        if (isset($_POST['mail']) && isset($_POST['password'])) { //note : pourraient contenir des chaînes vides
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
+
+            $errors = user::validate_login($mail, $password);
+            if (empty($errors)) {
+                $this->log_user(user::get_user_by_mail($mail));
+            }
+        }
+        (new View("login"))->show(["mail" => $mail, "password" => $password, "errors" => $errors]);
+    }
+
+    
 
 }
--- model/user.php
+++ model/user.php
@@ -6,8 +6,41 @@ class user extends Model {
 
 
 
-    public function __construct(public string $mail, public string $hashed_password, public string $full_name, public string $role , public ?string $iban = null) {
+    public function __construct(public string $mail, public string $hashed_password) {
+        
+        echo "LOGIN WORK NOW<br>";
+        echo "LOGIN DONE <br>";
     }
+
+    private static function check_password(string $clear_password, string $hash) : bool {
+        return $hash === Tools::my_hash($clear_password);
+    }
+
+    public static function validate_login(string $mail, string $password) : array {
+        $errors = [];
+        $user = user::get_user_by_mail($mail);
+        if ($user) {
+            if (!self::check_password($password, $user->hashed_password)) {
+                $errors[] = "Wrong password. Please try again.";
+            }
+        } else {
+            $errors[] = "Can't find a member with the email '$mail'. Please sign up.";
+        }
+        return $errors;
+    }
+    public static function get_user_by_mail(string $mail) : user|false {
+        $query = self::execute("SELECT * FROM users where mail = :mail", ["mail"=>$mail]);
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) {
+            return false;
+        } else {
+           
+            return new user($data["mail"], $data["hashed_password"]);
+        }
+
+    }
+
+
 
 
 }
