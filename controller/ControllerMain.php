9e655d8ea5fc1941530b5f852ea4e4de3203f43b
Yassine Houari <yassinehouari2001@gmail.com>
2022-12-19 13:30:18 +0100

    feat_UC_Singup : ajouter une view

--- controller/ControllerMain.php
+++ controller/ControllerMain.php
@@ -6,7 +6,7 @@ require_once 'framework/Controller.php';
 
 class ControllerMain extends Controller {
     public function index() : void {
-        echo "<h1>Hey !</h1>";
+        echo "<h1>Heyjj!</h1>";
     }
 
 }
