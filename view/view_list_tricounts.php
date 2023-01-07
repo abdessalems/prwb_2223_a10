e150364b94e05b9faf2448c8c8a5a76b2d3092aa
Baganda49 <abdessalemsaa@gmail.com>
2023-01-07 02:44:44 +0100

    style of liste tricount

--- view/view_list_tricounts.php
+++ view/view_list_tricounts.php
@@ -2,37 +2,39 @@
 <html>
 <style>
 </style>
-    <head>
-        <meta charset="UTF-8">
-        <title>users</title>
-        <base href="<?= $web_root ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-    </head>
-    <body>
-        <div  class="title"><h3>Your Tricounts <h3/> <input type="submit" value="Add"/>
-        </div>
-        <div class="main">
-            <table>
+<head>
+    <meta charset="UTF-8">
+    <title>Members</title>
+    <base href="<?= $web_root ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div  class="title"><h3>Your Tricounts <h3/>
+        <a href="your link for add tricount here yassin">Add </a>
 
-                <?php foreach ($tricounts as $tricount): ?>
-                    <tr>
-                  <th style="width:50%"><h5> <?=  $tricount->title ?>  </h5></th>
-                    </tr>
-                    <tr>
-                        <td> <?= $tricount->description ?></td>
-                       <td><?php if ($tricount->nb_participant== 1){
-                               echo " &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp You're alone ";
-                           }else if ($tricount->nb_participant== 2) {
-                               echo  " &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp with " , $tricount->nb_participant -1, "  Friend"   ;
-                           } else {
-                               echo  " &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp with " , $tricount->nb_participant -1, "  Friends"   ;
-                           }
-                           ?></td>
-                    </tr>
-                    <?php endforeach; ?>
-            </table>
+</div>
+<div class="main">
+    <table>
 
+        <?php foreach ($tricounts as $tricount): ?>
+            <tr>
+                <th> <?=  $tricount->title ?>  </th>
+                <th>     </th>
+            </tr>
+            <tr>
+                <td> <?= $tricount->description ?></td>
+                <td><?php if ($tricount->nb_participant< 2){
+                        echo " You're alone ";
+                    }else if ($tricount->nb_participant== 2) {
+                        echo  " with " , $tricount->nb_participant -1, "  Friend"   ;
+                    } else {
+                        echo  "  with " , $tricount->nb_participant -1, "  Friends"   ;
+                    }
+                    ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
             <a href="settings/settings">Settings</a>
         </div>
     </body>
