4fa2e6b04eb8b8bd51f7ab28e9c992222cb53c74
Baganda49 <abdessalemsaa@gmail.com>
2023-01-23 16:43:43 +0100

    ligne

--- controller/ControllerUser.php
+++ controller/ControllerUser.php
@@ -5,30 +5,34 @@ require_once 'framework/View.php';
 require_once 'framework/Controller.php';
 require_once 'model/tricount.php';
 
-class ControllerUser extends Controller {
-    
+class ControllerUser extends Controller
+{
+
 
     //page d'accueil. 
-    public function index() : void {
+    public function index(): void
+    {
         $this->profile();
     }
 
-    public function tricount() : void{
+    public function tricount(): void
+    {
         $user = $this->get_user_or_redirect();
         $tricounts = $this->get_tricount($user);
         //$nbr_Participent_Tricount = $this->get_nbr_Participent_Tricount($tricounts);
-        (new View('list_tricounts'))->show(['user'=> $user,"tricounts" => $tricounts] );
+        (new View('list_tricounts'))->show(['user' => $user, "tricounts" => $tricounts]);
     }
 
-     public function get_nbr_Participent_Tricount (tricount $tricount) : int {
-        return tricount::Participent_Tricount($tricount) ;
-     }
+    public function get_nbr_Participent_Tricount(tricount $tricount): int
+    {
+        return tricount::Participent_Tricount($tricount);
+    }
 
-    public function get_tricount(user $user): array{
+    public function get_tricount(user $user): array
+    {
 
         return tricount::get_tricounts($user);
     }
-
 
 
 }
