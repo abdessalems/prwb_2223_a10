212dcbd9564434f1274d26da475bef83eb8d3185
2209yahouari <yassinehouari2001gmail.com>
2023-01-25 14:42:08 +0100

    mofidiaction constructeur

--- model/tricount.php
+++ model/tricount.php
@@ -5,10 +5,10 @@ require_once "framework/Model.php";
 class tricount extends Model {
 
 
-    public function __construct(public string $title,  public ?string $description = null,
+    /*public function __construct(public string $title,  public ?string $description = null,
     public string $created_at , public int $creator,) {
 
-    }
+    }*/
 
 
 }
