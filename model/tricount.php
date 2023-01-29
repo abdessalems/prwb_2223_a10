7d286033580ac13c945d39aea74d8de68004bf72
Baganda49 <abdessalemsaa@gmail.com>
2023-01-29 15:00:43 +0100

    fix (develop_model user) : correction de conflits

--- model/tricount.php
+++ model/tricount.php
@@ -105,7 +105,7 @@ class tricount extends Model
             }
 
 
-   
+
 }
 
 
--- model/user.php
+++ model/user.php
@@ -3,23 +3,12 @@
 require_once "framework/Model.php";
 require_once 'model/Tricount.php';
 
-<<<<<<< HEAD
-=======
-class user extends Model
-{
->>>>>>> feat_UC_edit_operation
-
 class user extends Model
 {
 
-<<<<<<< HEAD
-    public function __construct(public int $id,public string $mail, public string $hashed_password, public string $full_name, public ?string $iban )
-    {
-    }
-=======
-    public function __construct(public int $id, public string $mail, public string $hashed_password, public string $full_name, public float $amount, public ?int $weight = null)
-    {
->>>>>>> feat_UC_edit_operation
+
+    public function __construct(public int $id, public string $mail, public string $hashed_password, public string $full_name, public ?string $iban , public ?float $amount =null, public ?int $weight = null)
+    {}
 
 
     private static function check_password(string $clear_password, string $hash): bool
@@ -27,7 +16,6 @@ class user extends Model
         return $hash === Tools::my_hash($clear_password);
     }
 
-<<<<<<< HEAD
     public static function validate_password_change_Pass(string $mail, string $current_password, string $new_password, string $confirm_password): array
     {
         $errors = [];
@@ -42,7 +30,8 @@ class user extends Model
             $errors [] = "the current password must be diffirent from the old one  . ";
         }
         return $errors;
-=======
+
+    }
     public static function get_amount_operations(operation $operation, int $nbr_repartition): array
     {
         $operation_amount = $operation->amount;
@@ -56,19 +45,14 @@ class user extends Model
         }
         foreach ($data as $row) {
             $amount_for_this_person = ($operation_amount / $total_weight) * ($row['weight']);
-            $operations_with_amount[] = new user($row['id'], $row['mail'], $row['hashed_password'], $row['full_name'], $amount_for_this_person, $row['weight']);
+
+            $operations_with_amount[] = new user($row['id'], $row['mail'], $row['hashed_password'], $row['full_name'],$row['iban'], $amount_for_this_person, $row['weight']);
         }
         return $operations_with_amount;
 
     }
 
 
-    private static function check_password(string $clear_password, string $hash): bool
-    {
-        // echo Tools::my_hash($clear_password);
-        return $hash === Tools::my_hash($clear_password);
->>>>>>> feat_UC_edit_operation
-    }
 
     public static function validate_login(string $mail, string $password): array
     {
@@ -89,11 +73,8 @@ class user extends Model
         return $errors;
     }
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> feat_UC_edit_operation
     public static function get_user_by_mail(string $mail): user|false
     {
         $query = self::execute("SELECT * FROM users where mail = :mail", ["mail" => $mail]);
@@ -101,12 +82,11 @@ class user extends Model
         if ($query->rowCount() == 0) {
             return false;
         } else {
-<<<<<<< HEAD
-            return new user($data["id"],$data["mail"],$data["hashed_password"],$data["full_name"], $data["iban"]);
-=======
 
-            return new user($data["id"], $data["mail"], $data["hashed_password"], $data["full_name"], 0);
->>>>>>> feat_UC_edit_operation
+            return new user($row['id'], $row['mail'], $row['hashed_password'], $row['full_name'],$row['iban']);
+
+            return new user($row['id'], $row['mail'], $row['hashed_password'], $row['full_name'],$row['iban'], 0,);
+
         }
 
     }
