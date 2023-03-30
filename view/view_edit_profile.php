e41142172f5fcdb5bd8491ce2ba81ba6eeb9ddd4
Baganda49 <abdessalemsaa@gmail.com>
2023-03-30 15:50:05 +0200

    fix (Settings) : edit profil settings

--- view/view_edit_profile.php
+++ view/view_edit_profile.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
-<style>
-</style>
 <head>
     <meta charset="UTF-8">
-    <title>view_operation</title>
+    <title> Edit profile </title>
     <base href="<?= $web_root ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="10">
     <!-- Favicons -->
     <link href="./assets/img/fav_icon.png" rel="icon">
     <link href="./assets/img/touch-icon.png" rel="apple-touch-icon">
@@ -27,76 +24,85 @@
 
     <!-- Template Main CSS File -->
     <link href="./assets/css/styles.css" rel="stylesheet">
-
 </head>
 <body>
-<div class="card-header">
-    <div class="d-flex w-100 justify-content-between">
-        <a class="btn btn-outline-danger"  href="operation/view_operation/<?= $operation->id ?>/<?= $id_user ?>">Cancel</a>
-        <h5 style="align-self: center " class="card-title" ><?= $tricount->title ?> > Edit expense <h5/>
-            <a class="btn btn-danger" href="operation/edit_operation/<?= $operation->id ?>/<?= $id_user ?>">Delete</a>
+
+<div class="main">
+    <div class="card-header">
+        <div >
+            <a  class="btn btn-outline-danger" href="settings/settings">Back</a>
+
+            <h5 class="card-title"  style="float:right;">Edit profile <h5/>
+        </div>
     </div>
+    <div class="card" style="display: block">
+
+        <div class ="card-body ">
+            <!-- Profile Edit Form -->
+            <form action="settings/edit_profile" method="post">
+                <div class="row mb-3">
+                    <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
+                    <div class="col-md-8 col-lg-9">
+                        <img src="./assets/img/profile-img.jpg" alt="Profile">
+                        <div class="pt-2">
+                            <a href="#" class="btn btn-primary btn-sm" title="Upload new profile image"><i class="bi bi-upload"></i></a>
+                            <a href="#" class="btn btn-danger btn-sm" title="Remove my profile image"><i class="bi bi-trash"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
+                    <div class="col-md-8 col-lg-9">
+                        <input name="full_name" type="text" class="form-control" id="fullName" value="<?= $user->full_name ?>">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
+                    <div class="col-md-8 col-lg-9">
+                        <input name="mail" type="email" class="form-control" id="Email" value="<?= $user->mail ?>" disabled>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="Iban" class="col-md-4 col-lg-3 col-form-label">IBAN</label>
+                    <div class="col-md-8 col-lg-9">
+                        <input name="iban" type="text" class="form-control" id="Iban" value="<?= $user->iban ?>">
+                    </div>
+                </div>
+
+
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+            </form><!-- End Profile Edit Form -->
+        </div>
+    </div>
+
+    <?php if (count($errors) != 0): ?>
+        <div class='errors'>
+            <br><br><p>Please correct the following error(s) :</p>
+            <ul>
+                <?php foreach ($errors as $error): ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
 </div>
 
-<div class="card">
-    <div class="card-body">
+<!-- Vendor JS Files -->
+<script src="./assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./assets/vendor/chart.js/chart.umd.js"></script>
+<script src="./assets/vendor/echarts/echarts.min.js"></script>
+<script src="./assets/vendor/quill/quill.min.js"></script>
+<script src="./assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="./assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="./assets/vendor/php-email-form/validate.js"></script>
 
-        <!-- Horizontal Form -->
-        <form action="operation/edit_operation/<?= $operation->id ?>/<?= $id_user ?>" method="post" >
-            <div class="row mb-3">
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="titlee" name="titlee" value="<?= $operation->title ?>">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" id="amount" name="amount" value="<?= $operation->amount ?> € ">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-sm-10">
-                    <label for="date">Date</label>
-                    <input type="date" class="form-control" id="date" name="date" value="<?= $operation->operation_date ?>">
-                </div>
-            </div>
-            <div class="row mb-3">
-                <div class="col-sm-10">
-                    <label class="form-label" for="Paid">Paid By</label>
-                    <select class="form-select" name="Paid" id="Paid">
-                        <?php foreach ($operation_amount as $o): ?>
-                            <option value="<?= $o->id ?>"><?= $o->full_name ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                </div>
-            </div>
-
-
-            <div class="row mb-3">
-                <div class="col-sm-10">
-                    <p>For Whom ? (select at least one ) </p>
-
-                    <table class="table table-borderless datatable">
-                        <tbody>
-                        <?php foreach ($operation_amount as $o): ?>
-                            <tr>
-                                <td> <input  type="checkbox" id="<?= $o->full_name ?>" name="<?= $o->full_name ?>" checked></td>
-                                <td> <label class="form-label" for="<?= $o->full_name ?>"><?= $o->full_name ?></label></td>
-                                <td>  <input type="number" class="form-control" id="weight" name="weight" value="<?= $o->weight ?>" min="1" max="10"></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </table>
-
-                </div>
-            </div>
-
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">Save</button>
-                <button type="reset" class="btn btn-outline-secondary">Reset</button>
-            </div>
-        </form><!-- End Horizontal Form -->
-    </div>
-</div>
-
+<!-- Template Main JS File -->
+<script src="./assets/js/main_admin_temp.js"></script>
 
 </body>
 </html>
